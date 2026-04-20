53b05c6fc6f82e43c5e732e0cd7fa99f22af7b4a
Abishekcs <abishekdascs@gmail.com>
2026-04-20 23:18:02 +0530

    Change Worker From 1 to 4 and also keep only one Rage Deffered task for testing

--- config/application.rb
+++ config/application.rb
@@ -6,19 +6,9 @@ Bundler.require(*Rage.groups)
 
 require "rage/all"
 
-# config/application.rb or config/environments/<environment>.rb
-
-# Rage.configure do
-#   config.deferred.schedule do
-#     every 1.hour, task: CleanupExpiredInvites
-#     every 1.minute, task: ResetCache
-#   end
-# end
-
 Rage.configure do
   config.deferred.schedule do
     every 1.minute, task: SayHello
-    every 1.minute, task: 'SayHello101'
   end
   # use this to add settings that are constant across all environments
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rage.configure do
   # Specify the number of server processes to run. Defaults to number of CPU cores.
-  config.server.workers_count = 1
+  config.server.workers_count = 4
 
   # Specify the port the server will listen on.
   config.server.port = 3000
